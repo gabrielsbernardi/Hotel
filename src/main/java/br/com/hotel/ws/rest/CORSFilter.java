979d85794ee5128695398710b13708896a84bf49
Gabriel Bernardi <gabriel.s.bernardi@gmail.com>
2019-05-02 10:14:18 -0300

    Config

--- src/main/java/br/com/hotel/ws/rest/CORSFilter.java
+++ src/main/java/br/com/hotel/ws/rest/CORSFilter.java
@@ -7,14 +7,19 @@ import com.sun.jersey.spi.container.ContainerResponseFilter;
 public class CORSFilter implements ContainerResponseFilter {
 
 	@Override
-	public ContainerResponse filter(ContainerRequest request, ContainerResponse response) {
+	public ContainerResponse filter(ContainerRequest request, ContainerResponse responseContext) {
 		
-		response.getHttpHeaders().putSingle("Access-Control-Allow-Origin", "https://hotel-bernardi-front.herokuapp.com/");
-		response.getHttpHeaders().putSingle("Access-Control-Allow-Credentials", "true");
-		response.getHttpHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD");
-		response.getHttpHeaders().putSingle("Access-Control-Allow-Headers", "origin, content-type, accept, Access-Control-Allow-Origin");
+		responseContext.getHttpHeaders().add("Access-Control-Allow-Origin", "*");
+	    responseContext.getHttpHeaders().add("Access-Control-Allow-Credentials", "true");
+	    responseContext.getHttpHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+	    responseContext.getHttpHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
 		
-		return response;
+//		response.getHttpHeaders().putSingle("Access-Control-Allow-Origin", "*");
+//		response.getHttpHeaders().putSingle("Access-Control-Allow-Credentials", "true");
+//		response.getHttpHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD");
+//		response.getHttpHeaders().putSingle("Access-Control-Allow-Headers", "origin, content-type, accept, Access-Control-Allow-Origin");
+		
+		return responseContext;
 	}
 
 }
