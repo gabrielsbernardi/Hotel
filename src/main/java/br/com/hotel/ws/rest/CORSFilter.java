6263695226b82548b96f86039ba31b165a9e9f3d
Gabriel Bernardi <gabriel.s.bernardi@gmail.com>
2019-05-02 09:22:46 -0300

    Cors

--- src/main/java/br/com/hotel/ws/rest/CORSFilter.java
+++ src/main/java/br/com/hotel/ws/rest/CORSFilter.java
@@ -9,7 +9,7 @@ public class CORSFilter implements ContainerResponseFilter {
 	@Override
 	public ContainerResponse filter(ContainerRequest request, ContainerResponse response) {
 		
-		response.getHttpHeaders().putSingle("Access-Control-Allow-Origin", "*");
+		response.getHttpHeaders().putSingle("Access-Control-Allow-Origin", "https://hotel-bernardi-front.herokuapp.com/");
 		response.getHttpHeaders().putSingle("Access-Control-Allow-Credentials", "true");
 		response.getHttpHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD");
 		response.getHttpHeaders().putSingle("Access-Control-Allow-Headers", "origin, content-type, accept, Access-Control-Allow-Origin");
